012c6362f79097d85ea4e361e642b77e503214b2
ggrubich <g.grubich.898@studms.ug.edu.pl>
2020-01-16 22:37:36 +0100

    Reimplement the buffer
    This commit reimplements the buffer using ring buffer data structure.
    It also changes its API to a more convenient one and documents it.

--- buffer.c
+++ buffer.c
@@ -3,14 +3,22 @@
 
 #include "buffer.h"
 
-const size_t DEFAULT_CAP = 64;
+static size_t min(size_t a, size_t b)
+{
+	return a < b ? a : b;
+}
+
+static size_t max(size_t a, size_t b)
+{
+	return a > b ? a : b;
+}
 
 void buffer_init(struct buffer *buf)
 {
 	buf->data = NULL;
 	buf->cap = 0;
-	buf->off = 0;
-	buf->len = 0;
+	buf->head = 0;
+	buf->tail = 0;
 }
 
 void buffer_finalize(struct buffer *buf)
@@ -18,72 +26,96 @@ void buffer_finalize(struct buffer *buf)
 	free(buf->data);
 }
 
+size_t buffer_len(struct buffer *buf)
+{
+	if (buf->head <= buf->tail) {
+		return buf->tail - buf->head;
+	} else {
+		return (buf->cap - buf->head) + buf->tail;
+	}
+}
+
+char buffer_get(struct buffer *buf, size_t i)
+{
+	return buf->data[(buf->head + i) % buf->cap];
+}
+
 int buffer_reserve(struct buffer *buf, size_t size)
 {
 	char *tmp;
-	int cap;
-	if (buf->cap - buf->len < size) {
-		cap = buf->cap;
-		do {
-			if (cap == 0) {
-				cap = DEFAULT_CAP;
-			} else {
-				// overflow
-				if (cap * 2 < cap) {
-					return -1;
-				}
-				cap = cap * 2;
-			}
-		} while (cap - buf->len < size);
-		tmp = malloc(cap);
-		if (!tmp) {
-			return -1;
-		}
-		memcpy(tmp, buf->data + buf->off, buf->len);
-		free(buf->data);
-		buf->data = tmp;
-		buf->cap = cap;
-		buf->off = 0;
-	} else if (buf->cap - buf->off - buf->len < size) {
-		memmove(buf->data, buf->data + buf->off, buf->len);
-		buf->off = 0;
+	size_t ncap;
+	// we reserve size+1 instead of size to avoid a situation where
+	// (tail + size) % cap == head
+	// which would indicate that the buffer is empty.
+	// one additional slot makes sure this will never happen.
+	size = size + 1;
+	if (buf->cap - buffer_len(buf) >= size) {
+		return 0;
+	}
+	ncap = max(2 * buf->cap, buffer_len(buf) + size);
+	tmp = realloc(buf->data, ncap);
+	if (!tmp) {
+		return -1;
+	}
+	if (buf->tail < buf->head) {
+		// the new buffer is at least two times larger,
+		// so this will always fit
+		memcpy(tmp + buf->cap, tmp, buf->tail);
+		buf->tail = buf->cap + buf->tail;
+	}
+	buf->data = tmp;
+	buf->cap = ncap;
+	return 0;
+}
+
+int buffer_push(struct buffer *buf, char *src, size_t len)
+{
+	size_t first, second;
+	if (buffer_reserve(buf, len) < 0) {
+		return -1;
+	}
+	first = min(len, buf->cap - buf->tail);
+	second = len - first;
+	memcpy(buf->data + buf->tail, src, first);
+	memcpy(buf->data, src + first, second);
+	buf->tail = (buf->tail + len) % buf->cap;
+	return 0;
+}
+
+int buffer_peek(struct buffer *buf, char *dst, size_t len)
+{
+	size_t first, second;
+	if (buffer_len(buf) < len) {
+		return -1;
+	}
+	if (dst) {
+		first = min(len, buf->cap - buf->head);
+		second = len - first;
+		memcpy(dst, buf->data + buf->head, first);
+		memcpy(dst + first, buf->data, second);
 	}
 	return 0;
 }
 
-char *buffer_front(struct buffer *buf)
+int buffer_pop(struct buffer *buf, char *dst, size_t len)
 {
-	return buf->data + buf->off;
-}
-
-char *buffer_back(struct buffer *buf)
-{
-	return buffer_front(buf) + buf->len;
-}
-
-void buffer_pop(struct buffer *buf, size_t n)
-{
-	if (n > buf->len) {
-		n = buf->len;
+	if (buffer_peek(buf, dst, len) < 0) {
+		return -1;
 	}
-	buf->off += n;
-	buf->len -= n;
+	buf->head = (buf->head + len) % buf->cap;
+	return 0;
 }
 
-void buffer_push(struct buffer *buf, size_t n)
+int buffer_append(struct buffer *buf, struct buffer *src, size_t len)
 {
-	if (n > buf->cap - buf->off - buf->len) {
-		n = buf->cap - buf->off - buf->len;
+	size_t first, second;
+	if (buffer_len(src) < len || buffer_reserve(buf, len) < 0) {
+		return -1;
 	}
-	buf->len += n;
-}
-
-size_t buffer_len(struct buffer *buf)
-{
-	return buf->len;
-}
-
-size_t buffer_rem(struct buffer *buf)
-{
-	return buf->cap - buf->off - buf->len;
+	first = min(len, src->cap - src->head);
+	second = len - first;
+	buffer_push(buf, src->data + src->head, first);
+	buffer_push(buf, src->data, second);
+	src->head = (src->head + len) % src->cap;
+	return 0;
 }
--- buffer.h
+++ buffer.h
@@ -3,27 +3,70 @@
 #include <stddef.h>
 
 struct buffer {
-	// private
+	// those fields should be considered private
+
 	char *data;
+	// cap is the number of bytes allocated for data.
 	size_t cap;
-	size_t off;
-	size_t len;
+	// head is the index of the first byte present
+	// and tail is the index to which the next byte should be written.
+	// head == tail implies that the buffer is empty.
+	size_t head;
+	size_t tail;
 };
 
+/* Initializes the buffer.
+ */
 void buffer_init(struct buffer *buf);
 
+/* Frees all resources allocated by the buffer.
+ */
 void buffer_finalize(struct buffer *buf);
 
-int buffer_reserve(struct buffer *buf, size_t size);
-
-char *buffer_front(struct buffer *buf);
-
-char *buffer_back(struct buffer *buf);
-
-void buffer_pop(struct buffer *buf, size_t n);
-
-void buffer_push(struct buffer *buf, size_t n);
-
+/* Returns the number of bytes in the buffer.
+ */
 size_t buffer_len(struct buffer *buf);
 
-size_t buffer_rem(struct buffer *buf);
+/* Returns the ith byte in the buffer.
+ */
+char buffer_get(struct buffer *buf, size_t i);
+
+/* Ensures that the buffer will be able to store size additional bytes
+ * without resizing.
+ *
+ * Returns 0 on success and -1 on failure.
+ */
+int buffer_reserve(struct buffer *buf, size_t size);
+
+/* Adds len bytes from src to the back of the buffer. The buffer will
+ * be resized if necessary.
+ *
+ * If the resizing fails, buffer_push will return -1 without modifying
+ * the buffer. Otherwise 0 will be returned.
+ */
+int buffer_push(struct buffer *buf, char *src, size_t len);
+
+/* Copies len bytes from the front of the buffer to dst.
+ *
+ * If length of the buffer is lower than the requested number of bytes,
+ * buffer_pop will return -1. Otherwise 0 will be returned.
+ */
+int buffer_peek(struct buffer *buf, char *dst, size_t len);
+
+/* Removes len bytes from the front of the buffer and stores them in dst.
+ * If dst is NULL buffer_pop will just remove the bytes.
+ *
+ * If length of the buffer is lower than the requested number of bytes,
+ * buffer_pop will return -1 without modifying the buffer.
+ * Otherwise 0 will be returned.
+ */
+int buffer_pop(struct buffer *buf, char *dst, size_t len);
+
+/* Removes len bytes from the front of src and adds them to the back of buf.
+ * buf will be resized if necessary.
+ *
+ * If length of src is lower than requested number of bytes or if resizing
+ * of buf fails, buffer_append will return -1 without modifying the buffers.
+ * Otherwise 0 will be returned.
+ */
+int buffer_append(struct buffer *buf, struct buffer *src, size_t len);
