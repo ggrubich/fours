147c26c65fbea5c2d914883e1971e1d7eb920582
ggrubich <g.grubich.898@studms.ug.edu.pl>
2020-01-25 05:07:09 +0100

    Document the client states

--- client_common.h
+++ client_common.h
@@ -29,13 +29,37 @@ struct event {
 };
 
 enum client_state {
+	// Client tried to log in and is waiting for server's response.
+	// next states: STATE_LOGIN_ERR or STATE_LOBBY
 	STATE_LOGIN_WAIT,
+
+	// Login failed, display the error message.
+	// next states: none
 	STATE_LOGIN_ERR,
+
+	// The main menu.
+	// next states: STATE_START_WAIT
 	STATE_LOBBY,
+
+	// Client is waiting for the game to start.
+	// next states: STATE_GAME or STATE_LOBBY
 	STATE_START_WAIT,
+
+	// Client is in the game.
+	// next states: STATE_GAME_QUIT, STATE_GAME_OVER or STATE_HALTED
 	STATE_GAME,
+
+	// Client is still in the game, but it is displaying a popup asking
+	// if it should quit the game or not.
+	// next states: STATE_LOBBY, STATE_GAME, STATE_GAME_OVER or STATE_HALTED
 	STATE_GAME_QUIT,
+
+	// Game has ended, display the results.
+	// next states: STATE_LOBBY
 	STATE_GAME_OVER,
+
+	// The opponent has quit the game, display the error.
+	// next states: STATE_LOBBY
 	STATE_HALTED,
 };
 
@@ -52,13 +76,18 @@ enum {
 };
 
 struct game_base {
+	// name of the other player
 	char *other;
+	// our side
 	enum side side;
 	enum side **board;
 	int width;
 	int height;
+	// currently selected column
 	int column;
+	// current turn
 	enum side turn;
+	// available undos for each player
 	int red_undos;
 	int blue_undos;
 };
